642716d18852f97a79676fca22bed21443fe8c00
Vitor Melo <melo.vitor2001@gmail.com>
2025-06-20 23:07:45 -0300

    Updated main.tf (gcp)

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,6 +1,7 @@
 ###############################
 # Google Cloud Storage bucket
 ###############################
+
 resource "google_storage_bucket" "face_images" {
   name     = var.bucket_name
   location = var.gcp_region
@@ -23,6 +24,7 @@ resource "google_storage_bucket" "face_images" {
 ###############################
 # Pub/Sub topic
 ###############################
+
 resource "google_pubsub_topic" "face_registration" {
   name = var.pubsub_topic_name
 }
@@ -30,15 +32,17 @@ resource "google_pubsub_topic" "face_registration" {
 ###############################
 # Firestore database
 ###############################
+
 resource "google_firestore_database" "db" {
   name        = var.firestore_database
   location_id = var.gcp_region
-  type        = "NATIVE"
+  type        = "FIRESTORE_NATIVE"
 }
 
 ###############################
 # Service Accounts
 ###############################
+
 resource "google_service_account" "register_sa" {
   account_id   = "register-face-func-sa"
   display_name = "Register Face Function SA"
@@ -52,51 +56,88 @@ resource "google_service_account" "recognize_sa" {
 ###############################
 # IAM Bindings for Register SA
 ###############################
+
 resource "google_project_iam_member" "register_storage" {
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.register_sa.email}"
+  project = var.gcp_project_id
 }
-resource "google_project_iam_member" "register_vision" {
-  role   = "roles/vision.user"
-  member = "serviceAccount:${google_service_account.register_sa.email}"
-}
+
+# resource "google_project_iam_member" "register_vision" {
+#   role   = "roles/vision.user"
+#   member = "serviceAccount:${google_service_account.register_sa.email}"
+#   project = var.gcp_project_id
+# }
+
 resource "google_project_iam_member" "register_firestore" {
   role   = "roles/datastore.user"
   member = "serviceAccount:${google_service_account.register_sa.email}"
+  project = var.gcp_project_id
 }
+
 resource "google_project_iam_member" "register_pubsub" {
   role   = "roles/pubsub.publisher"
   member = "serviceAccount:${google_service_account.register_sa.email}"
+  project = var.gcp_project_id
+}
+
+resource "google_cloudfunctions_function_iam_member" "register_invoker" {
+  project        = var.gcp_project_id
+  region         = var.gcp_region
+  cloud_function = google_cloudfunctions_function.register_face.name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "allUsers"
 }
 
 ###############################
 # IAM Bindings for Recognize SA
 ###############################
+
 resource "google_project_iam_member" "recognize_storage" {
   role   = "roles/storage.objectViewer"
   member = "serviceAccount:${google_service_account.recognize_sa.email}"
+  project = var.gcp_project_id
 }
-resource "google_project_iam_member" "recognize_vision" {
-  role   = "roles/vision.user"
-  member = "serviceAccount:${google_service_account.recognize_sa.email}"
-}
+
+# resource "google_project_iam_member" "recognize_vision" {
+#   role   = "roles/vision.user"
+#   member = "serviceAccount:${google_service_account.recognize_sa.email}"
+#   project = var.gcp_project_id
+# }
+
 resource "google_project_iam_member" "recognize_firestore" {
   role   = "roles/datastore.viewer"
   member = "serviceAccount:${google_service_account.recognize_sa.email}"
+  project = var.gcp_project_id
+}
+
+resource "google_cloudfunctions_function_iam_member" "recognize_invoker" {
+  project        = var.gcp_project_id
+  region         = var.gcp_region
+  cloud_function = google_cloudfunctions_function.recognize_face.name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "allUsers"
+}
+
+resource "google_project_iam_member" "recognize_storage_writer" {
+  project = var.gcp_project_id
+  role    = "roles/storage.objectCreator"   # ou "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.recognize_sa.email}"
 }
 
 ###############################
 # Package Cloud Functions
 ###############################
+
 data "archive_file" "register_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../gcp_functions"
+  source_dir  = "${path.module}/../../gcp_functions/register"
   output_path = "${path.module}/register_face_deployment.zip"
 }
 
 data "archive_file" "recognize_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../../gcp_functions"
+  source_dir  = "${path.module}/../../gcp_functions/recognize"
   output_path = "${path.module}/recognize_face_deployment.zip"
 }
 
@@ -115,6 +156,7 @@ resource "google_storage_bucket_object" "recognize_archive" {
 ###############################
 # Cloud Functions
 ###############################
+
 resource "google_cloudfunctions_function" "register_face" {
   name        = "registerFaceFunction"
   runtime     = var.gcp_function_runtime
@@ -155,26 +197,36 @@ resource "google_cloudfunctions_function" "recognize_face" {
 ###############################
 # API Gateway
 ###############################
+
 resource "google_api_gateway_api" "face_api" {
+  provider = google-beta
   api_id = "face-recognition-api"
 }
 
 data "template_file" "openapi" {
   template = <<EOT
-openapi: 2.0
+swagger: '2.0'
 info:
   title: FaceRecognitionAPI
   version: 1.0.0
+
+host: "${var.gcp_project_id}.cloudfunctions.net"
+schemes:
+  - https
+
 paths:
   /register:
     post:
+      operationId: registerFace
       x-google-backend:
         address: ${google_cloudfunctions_function.register_face.https_trigger_url}
       responses:
         '200':
           description: OK
+
   /recognize:
     post:
+      operationId: recognizeFace
       x-google-backend:
         address: ${google_cloudfunctions_function.recognize_face.https_trigger_url}
       responses:
@@ -184,22 +236,27 @@ EOT
 }
 
 resource "google_api_gateway_api_config" "face_config" {
-  api       = google_api_gateway_api.face_api.id
-  config_id = "v1"
+  provider      = google-beta
+  api           = google_api_gateway_api.face_api.api_id
+  api_config_id = "v1"
+
   openapi_documents {
     document {
       path     = "openapi.yaml"
-      contents = data.template_file.openapi.rendered
+      contents = base64encode(data.template_file.openapi.rendered)
     }
   }
+
   depends_on = [
     google_cloudfunctions_function.register_face,
-    google_cloudfunctions_function.recognize_face
+    google_cloudfunctions_function.recognize_face,
   ]
 }
 
 resource "google_api_gateway_gateway" "face_gateway" {
-  api        = google_api_gateway_api.face_api.id
-  api_config = google_api_gateway_api_config.face_config.id
-  location   = var.gcp_region
+  provider    = google-beta
+  project     = var.gcp_project_id
+  gateway_id  = "face-recognition-gateway"
+  api_config  = google_api_gateway_api_config.face_config.id
+  region      = var.gcp_region
 }
