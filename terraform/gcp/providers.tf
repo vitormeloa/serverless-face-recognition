ebfe5010471c2ea305675fcd3edb11dd07f19485
Vitor Melo <melo.vitor2001@gmail.com>
2025-06-20 23:08:20 -0300

    Updated providers.tf (gcp)

--- terraform/gcp/providers.tf
+++ terraform/gcp/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.0"
+    }
     archive = {
       source  = "hashicorp/archive"
       version = "~> 2.2"
@@ -16,17 +20,24 @@ provider "google" {
   region  = var.gcp_region
 }
 
-# Enable necessary APIs
+provider "google-beta" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+
 resource "google_project_service" "services" {
   for_each = toset([
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
     "cloudfunctions.googleapis.com",
     "cloudbuild.googleapis.com",
     "storage.googleapis.com",
-    "cloudvision.googleapis.com",
+    "vision.googleapis.com",
     "firestore.googleapis.com",
     "pubsub.googleapis.com",
     "apigateway.googleapis.com"
   ])
+
   service            = each.key
   disable_on_destroy = false
 }
